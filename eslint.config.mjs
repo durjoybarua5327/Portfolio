6fffd17d45886fed20a9450093994c7738f7ccb8
Durjoy Barua <durjoybarua8115@gmail.com>
2026-01-14 13:43:52 +0600

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,8 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Add source files to ignore list to skip linting during build (similar to ignoreDuringBuilds: true)
+    "src/**"
   ]),
 ]);
 
