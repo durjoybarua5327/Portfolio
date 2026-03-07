6d05e0d980e31b7fcac33a1662816b6da238a92f
Durjoy Barua <durjoybarua8115@gmail.com>
2026-03-08 00:35:40 +0600

    gg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'awxxyqrsogmocclpoeit.supabase.co',
+        port: '',
+        pathname: '/**', // Broad path support
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
